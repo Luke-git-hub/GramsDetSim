ac920bef171f3d6c13c35c9efd8db43b29c26d83
aconover42 <85565110+aconover42@users.noreply.github.com>
2021-07-23 10:25:42 -0400

    update July 23rd, 2021
    remaining issues include code annotation and accounting for deviations in the electric field

--- grams_helpers.cpp
+++ grams_helpers.cpp
@@ -11,23 +11,76 @@
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RVec.hxx>
 
-// recombination needs more geometry, nrg and field are function of x y, z and t (thanks LArHits)
-// specify where in the step it occurs
-// electric field table? doesn't need super fine resolution
-// finish up recombination, run an ntuple through it, make plots to show there is a difference between recomb on or off, and it makes sense
+// copy these into the main GramsDetSim routine, then all you should need to do is include code to read the ntuple
+// charge_recombination performs the actual calculation of recombination (returning the fraction that survived, not the energy of that fraction)
+// recombination_vector performs the charge_recombination calculation for a whole vector of inputs, and writes it to a new output vector
 
-double field_lookup (double x, double y, double z, double t)
+double charge_recombination(double dEdx)
+// Takes input of the change in energy across the distance travelled in a single step by the particle
+// source for this equation is eqn 2.4 in the 2013 paper "A study of electron recombination using highlyionizing particles in the ArgoNeuT Liquid ArgonTPC"
+// this modified box model is used because it works for all ranges of dE/dx as well as not having the technical difficulties that arise when applying the birks model to highly ionizing particles
 {
-  return 0 //replace with actual field function when possible
+  double field; //electric field on the cloud of ionized particles, varies with position due to space charge, which we don't have a model for yet
+  double new_dEdx //since geant4's units gives dEdx as MeV/mm, we need to convert it to MeV/cm
+  new_dEdx = dEdx * 10
+  double a = 0.930; //average of the a value for 20 to 90 degrees angle bins for proton sample detection in ArgoNeuT, unitless value
+  double b = 0.212; // average of the B value for 20 to 90 degrees angle bins for proton sample detection in ArgoNeuT, units of (kV*g)/(MeV*cm^3)
+  double rho = 1.3973; //density of liquid argon, from brookhaven LAr page, units of (g/cm^3)
+  double sigma; //multiple calculations for this are presented in the paper, we will be using eqn 8.1 to calculate sigma as it removes some dependencies
+  double total;
+  field = 1.0; //since we can't model space charge, let's just set it to a constant value (units are kV/cm)
+  //The following calculations are based off of the modified box model used in the ICARUS experiment, with constant values taken from the brookhaven page on liquid argon TPCs
+  //Be very specific in where this equation is from (what paper), what it is finding, what it is talking about
+  sigma = (b * dEdx) / (field * rho);
+  total = log(a + sigma) / sigma;
+  //returns a decimal value representing the fraction that has survived recombination
+  return total;
 }
 
-double energy_calculation(double x, double y, double z, double t)
+typedef const ROOT::VecOps::RVec<double> inVec_t;
+std::vector<double> recombination_vector(inVec_t dEdx)
 {
- return 0 //replace with actual energy function if possible
+  auto vectorSize = dEdx.size();
+  std::vector<double> recomb_output( vectorSize );
+  for (size_t i = 0; i != vectorSize, i++)
+  {
+    recomb_output[i] = charge_recombination(dEdx[i]);
+  }
+  return recomb_output;
 }
 
-double charge_recombination(double x, double y, double z, double t)
-// Takes input of x, y, and z location of particle in the detector, as well as the time
+
+int main()
+//for testing recombination
+//how to take in the TTree?
+{
+  std::string outputfile("gramsdetsim_recombination.root");
+  //take in TTree and TFile from GramsDetSim
+  ROOT::RDataFrame input_ntuple("GramsDetSim_ntuple", "NewOutput_GG4Level.root");
+  
+  //Alright, now let's calculate recombination. First we'll need some variables
+  double field; //electric field on the cloud of ionized particles, varies with position
+  double a = 0.930; //average of the a value for 20 to 90 degrees angle bins for proton sample detection in ArgoNeuT, unitless value
+  double b = 0.212; // average of the B value for 20 to 90 degrees angle bins for proton sample detection in ArgoNeuT, units of (kV*g)/(MeV*cm^3)
+  double rho = 1.3973; //density of liquid argon, from brookhaven LAr page, units of (g/cm^3)
+  double sigma; //multiple calculations for this are presented in the paper, we will be using eqn 8.1 to calculate sigma as it removes some dependencies
+  // ideally, we'll have a function for defining the electric field strength at points in the LArTPC. Until then, we'll have a field strength for the entire field  
+  double field = 1.0;
+  //now let's calculate recombination (how do I do that)
+  //also lucia might not be including ionization energy in her program so I'll need to calculate it
+  auto recombination = input_ntuple.Define("sigma","(b * dEdx) / (field * rho)").Define("recomb_rate", "log(a + sigma/sigma").Define("Energy_left = compt_Eion * recomb_rate");
+  
+  //need to process ntuple, pull out what I need
+  auto output = 0; //replace with way to combine inported ntuple with recombination
+  //return the output, either through copying Lucia's code or something else
+}
+
+
+// Depreciated code below -----------------------------------------------------
+
+/*
+//here is the origonal recombination function, not generalizable as it asks for manual entry of values
+double origonal_recombination()
 // source for this equation is eqn 2.4 in the 2013 paper "A study of electron recombination using highlyionizing particles in the ArgoNeuT Liquid ArgonTPC"
 // this modified box model is used because it works for all ranges of dE/dx as well as not having the technical difficulties that arise when applying the birks model to highly ionizing particles
 {
@@ -39,8 +92,10 @@ double charge_recombination(double x, double y, double z, double t)
   double sigma; //multiple calculations for this are presented in the paper, we will be using eqn 8.1 to calculate sigma as it removes some dependencies
   double total;
   //now we are going to find the electric field at the position of the compton electron
-  field = field_lookup(x, y, z, t);
-  dEdx = energy_calculation(x,y,z,t) ;//georgia says we need this, can we just use Bill's code?
+  std::cout << "Please enter the electric field in your LArTPC\n";
+  std::cin >> field;
+  std::cout << "Please enter the change in energy over the path length of the particle this step\n";
+  std::cin >> dEdx;
   //use a different model? need to justify coefficients
   //The following calculations are based off of the modified box model used in the ICARUS experiment, with constant values taken from the brookhaven page on liquid argon TPCs
   //Be very specific in where this equation is from (what paper), what it is finding, what it is talking about
@@ -48,6 +103,14 @@ double charge_recombination(double x, double y, double z, double t)
   total = log(a + sigma) / sigma;
   //returns a decimal value representing the fraction that has survived recombination
   return total;
+}
+
+
+int old_main()
+{
+  double recomb_value;
+  recomb_value = origonal_recombination();
+  std::cout << "Your recombination value is " << recomb_value << std::endl;
 }
 
 double charge_absorption (double I_nought, double distance)
@@ -61,16 +124,4 @@ double charge_absorption (double I_nought, double distance)
   intensity = I_nought * exp(power);
   return intensity;
 }
-
-int main()
-//for testing recombination
-{
-  std::string filename("gramsdetsim.root");
-  std::string outputfile("gramsdetsim_recombination.root");
-    //go through from ntuple.start to ntuple.end
-    double output;
-    auto input;
-    //need to process ntuple, pull out what I need
-    output = charge_recombination(input);
-    std::cout << output << std::endl;
-}
+*/
